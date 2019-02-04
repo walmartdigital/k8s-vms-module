4c0ece2d6e7e27b69ba41b28105dc0a2f857cae4
Max Guzman <maximiliano.guzman@walmart.com>
2019-02-04 09:18:55 -0300

    fix(terraform): output bastion private ip address.

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "bastion_ip" {
+output "bastion_public_ip" {
   value = "${azurerm_public_ip.bastion.ip_address}"
 }
+
+output "bastion_private_ip" {
+  value = "${azurerm_network_interface.bastion.private_ip_address}"
+}
+
 output "worker_ips" {
   value = "${join(",", azurerm_network_interface.worker.*.private_ip_address)}"
 }
